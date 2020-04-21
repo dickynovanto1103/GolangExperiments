52f04bd133a0d87c85857fc2ceee7cda976a0c6d
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-04-21 17:40:39 +0800

    Move the variable to main program

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	cloud.google.com/go/firestore v1.1.1 // indirect
 	firebase.google.com/go v3.12.0+incompatible
 	github.com/cucumber/godog v0.9.0
+	github.com/cucumber/messages-go/v10 v10.0.3
 	github.com/hashicorp/golang-lru v0.5.3 // indirect
 	github.com/robfig/cron v1.2.0
 	golang.org/x/net v0.0.0-20191027093000-83d349e8ac1a // indirect
--- godog/godog_test.go
+++ godog/godog_test.go
@@ -1,14 +1,13 @@
-package godog
+package main
 
 import (
 	"fmt"
 	"github.com/cucumber/godog"
+	"github.com/cucumber/messages-go/v10"
 )
 
-var godogs int
-
-func thereAreGodogs(arg1 int) error {
-	godogs = arg1
+func thereAreGodogs(available int) error {
+	godogs = available
 	return nil
 }
 
@@ -18,15 +17,18 @@ func iEat(arg1 int) error {
 }
 
 func thereShouldBeRemaining(arg1 int) error {
-	if godogs == arg1 {
-		return nil
+	if godogs != arg1 {
+		return fmt.Errorf("godog not the same should be %v instead of %v", arg1, godogs)
 	}
-	return fmt.Errorf("godog not the same should be %v instead of %v", arg1, godogs)
+	return nil
 }
 
 func FeatureContext(s *godog.Suite) {
 	s.Step(`^there are (\d+) godogs$`, thereAreGodogs)
 	s.Step(`^I eat (\d+)$`, iEat)
 	s.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
-}
 
+	s.BeforeScenario(func(pickle *messages.Pickle) {
+		godogs = 0 //reset
+	})
+}
