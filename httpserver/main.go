5e3b43f8bff910347ee86a462298cecf4ed3ae1a
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-04-25 18:15:27 +0800

    Add experiment of decode and marshall an array

--- httpserver/main.go
+++ httpserver/main.go
@@ -20,17 +20,31 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("r: %v body: %v form: %v", r, r.Body, r.Form)
 		log.Println("student: ", student)
 		w.WriteHeader(http.StatusOK) //the write Header will only be set once, and will not be assigned to the other value later if set with the other values
-		w.WriteHeader(http.StatusInternalServerError)
+		//w.WriteHeader(http.StatusInternalServerError) // if we set it twice, it will give warning of superfluous WriteHeader operation
 		path := r.URL.String()
 		log.Println("path: ", path)
-		w.Write([]byte("POST request successful, student: " + student.String()))
+		byteArr, _ := json.Marshal(student)
+
+		w.Write([]byte("POST request successful, student: " + string(byteArr)))
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
+func handlerArray(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var arr []int32
+	if err := decoder.Decode(&arr); err != nil {
+		log.Println("error decoding, err: ", err)
+	}
+	log.Println("arr: ", arr)
+	byteArr, _ := json.Marshal(arr)
+	log.Println("byteArr: ", string(byteArr))
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/arr", handlerArray)
 
-	log.Fatal(http.ListenAndServe(":8181", nil))
+	log.Fatal(http.ListenAndServe(":8183", nil))
 }
