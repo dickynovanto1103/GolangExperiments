20c6da2b2ecd2228c08b819a38e2fcbd0811e07f
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-04-25 13:50:53 +0800

    Add experiment of write header http status twice

--- httpserver/main.go
+++ httpserver/main.go
@@ -19,8 +19,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 		log.Printf("r: %v body: %v form: %v", r, r.Body, r.Form)
 		log.Println("student: ", student)
-
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusOK) //the write Header will only be set once, and will not be assigned to the other value later if set with the other values
+		w.WriteHeader(http.StatusInternalServerError)
+		path := r.URL.String()
+		log.Println("path: ", path)
 		w.Write([]byte("POST request successful, student: " + student.String()))
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
