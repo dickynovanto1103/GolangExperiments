295d1df9f2ae840d21dd40b8ae356c783c1af1d2
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-05-13 21:11:59 +0800

    Add parallel and serial goroutine exec experiment

--- goconcurrency/README.md
+++ goconcurrency/README.md
@@ -1,5 +1,6 @@
 # Go Concurrency Note
 1. If we have this piece of code
+
 ```
     ch := make(chan int)
     go func() {
@@ -15,4 +16,5 @@
 ```
 Since it request the value of the ch, then it will start to seek in all goroutines, which one that has sending to ch channel operation.
 The order of it is going from the latest goroutine created, till the first. So it's like stack. If all goroutines are observed and no send message to ch channel operation, then all goroutines are asleep and it will cause deadlock.
+
 2. From exp2() function, we can learn that once the main goroutines sleep time is over, it will continue to do the main flow, so the infinite loops in the function that the other goroutine is working on will stop.
--- goconcurrency/main.go
+++ goconcurrency/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"runtime"
 	"time"
 )
 
@@ -42,7 +44,101 @@ func exp2() {
 	time.Sleep(1*time.Second)
 }
 
+func CanWeDoParallelOperation() {
+	now := time.Now()
+	threads := runtime.GOMAXPROCS(0)
+	bil := int32(0)
+	ch := make(chan int, threads)
+
+	for i := 0; i < threads; i++ {
+		go func(threadNum int) {
+			//log.Println("processong thread num:", threadNum)
+			for i := 0; i < 100000000; i++ {
+				//atomic.AddInt32(&bil, 1)
+				bil++
+			}
+			//log.Printf("thread num: %v done\n", threadNum)
+			ch <- 1
+		}(i)
+	}
+
+	for i:=0;i<threads;i++ {
+		log.Println("done: ", i)
+		<-ch
+	}
+	log.Println("totBil: ", bil)
+	log.Println("time elapsed: ", time.Since(now))
+}
+
+func SerialAdd() {
+	now := time.Now()
+	var bil int32 = 0
+	threads := runtime.GOMAXPROCS(0)
+	for i:=0;i<threads;i++ {
+		for j:=0;j<100000000;j++ {
+			//atomic.AddInt32(&bil, 1)
+			bil++
+		}
+	}
+	log.Println("bil final: ", bil)
+	log.Println("time elapsed: ", time.Since(now))
+}
+var n = 10000000
+
+var arr1 = make([]int, n)
+var arr2 = make([]int, n)
+var sum = make([]int, n)
+
+func SumParallel(n int) {
+	now := time.Now()
+	threads := runtime.GOMAXPROCS(0)
+	ch := make(chan int, threads)
+	//wg := sync.WaitGroup{}
+	//wg.Add(threads)
+	for i:=0;i<threads;i++ {
+		go func(awal, akhir int) {
+			for i:=awal;i<akhir;i++ {
+				sum[i] = arr1[i] + arr2[i]
+			}
+			ch <- 1
+			//wg.Done()
+		}(i*n/threads, (i+1)*n/threads)
+	}
+
+	//wg.Wait()
+
+	for i:=0;i<threads;i++ {
+		<-ch
+	}
+
+	log.Println("parallel: ", time.Since(now))
+}
+
+func SumSerial(n int) {
+	now := time.Now()
+
+	for i:=0;i<n;i++{
+		sum[i] = arr1[i] + arr2[i]
+	}
+
+	log.Println("serial: ", time.Since(now))
+}
+
+func gen(n int) {
+	for i:=0;i<n;i++ {
+		arr1[i] = i
+		arr2[i] = i
+	}
+
+}
+
 func main() {
 	//exp1()
-	exp2()
+	//exp2()
+	gen(n)
+	log.Println("done")
+	SumSerial(n)
+	SumParallel(n)
+	//CanWeDoParallelOperation()
+	//SerialAdd()
 }
