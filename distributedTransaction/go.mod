2e842f478a32c7df8a521480ce2e944831ad018e
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-07-31 23:30:26 +0800

    Add experiment of go-saga, code still not working

---
+++ distributedTransaction/go.mod
@@ -0,0 +1,14 @@
+module distributedTransaction
+
+go 1.13
+
+require (
+	github.com/Shopify/sarama v1.26.4 // indirect
+	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f // indirect
+	github.com/juju/testing v0.0.0-20200706033705-4c23f9c453cd // indirect
+	github.com/lysu/go-saga v0.0.0-20160426172250-a5372c967a5b
+	github.com/lysu/kazoo-go v0.0.0-20160229162054-26744d16cdcf // indirect
+	github.com/samuel/go-zookeeper v0.0.0-20200724154423-2164a8ac840e // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+)
---
+++ distributedTransaction/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"github.com/lysu/go-saga"
+	"time"
+)
+
+func main() {
+	// 1. Define sub-transaction method, anonymous method is NOT required, Just define them as normal way.
+	DeduceAccount := func(ctx context.Context, account string, amount int) error {
+		// Do deduce amount from account, like: account.money - amount
+		return nil
+	}
+	CompensateDeduce := func(ctx context.Context, account string, amount int) error {
+		// Compensate deduce amount to account, like: account.money + amount
+		return nil
+	}
+	DepositAccount := func(ctx context.Context, account string, amount int) error {
+		// Do deposit amount to account, like: account.money + amount
+		return nil
+	}
+	CompensateDeposit := func(ctx context.Context, account string, amount int) error {
+		// Compensate deposit amount from account, like: account.money - amount
+		return nil
+	}
+
+	saga.StorageConfig.Kafka.ZkAddrs = []string{"0.0.0.0:2181"}
+	saga.StorageConfig.Kafka.BrokerAddrs = []string{"0.0.0.0:9092"}
+	saga.StorageConfig.Kafka.Partitions = 1
+	saga.StorageConfig.Kafka.Replicas = 1
+	saga.StorageConfig.Kafka.ReturnDuration = 50 * time.Millisecond
+
+	saga.AddSubTxDef("deduce", DeduceAccount, CompensateDeduce).
+		AddSubTxDef("deposit", DepositAccount, CompensateDeposit)
+	sagaId := uint64(2)
+	from, to := "foo", "bar"
+	amount := 100
+
+	saga.StartSaga(context.Background(), sagaId).
+		ExecSub("deduce", from, amount).
+		ExecSub("deposit", to, amount).
+		EndSaga()
+}
