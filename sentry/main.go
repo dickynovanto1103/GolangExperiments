edd66fdfa5415045845c0a6e2a10bab5bdbe5501
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-11-05 23:33:28 +0800

    Add Sentry Recover experiment

--- sentry/main.go
+++ sentry/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"errors"
 	"log"
 	"time"
 
 	"github.com/getsentry/sentry-go"
 )
+
+func experimentSentryRecover() {
+	defer func() {
+		if err := recover(); err != nil {
+			sentry.CurrentHub().Recover(err)
+		} else {
+			log.Printf("received nil err")
+			sentry.CurrentHub().Recover(err)
+		}
+
+		log.Printf("done recover")
+	}()
+
+	log.Printf("test panic")
+	cobaPanic("halo test panic")
+}
+
+func cobaPanic(msg string) {
+	panic(msg)
+}
 
 func main() {
 	err := sentry.Init(sentry.ClientOptions{
@@ -17,10 +36,10 @@ func main() {
 		log.Fatalf("error sentry init, err: %v", err)
 	}
 	log.Printf("no error")
+	experimentSentryRecover()
 
-	for i := 0; i < 1000000; i++ {
-		eventId := sentry.CaptureException(errors.New("hello error"))
-		log.Printf("eventId: %v", *eventId)
-	}
-	sentry.Flush(5 * time.Second)
+	//eventId := sentry.CaptureException(errors.New("hello error"))
+	//log.Printf("eventId: %v", *eventId)
+	defer sentry.Flush(5 * time.Second)
+	//log.Printf("done flushing")
 }
