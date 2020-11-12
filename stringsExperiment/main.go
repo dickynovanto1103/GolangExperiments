d868183e2e66e932ac96f1df8a3a29741f966162
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-11-12 09:18:01 +0800

    Add experiment strings.EqualFold

--- stringsExperiment/main.go
+++ stringsExperiment/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"strings"
+)
+
+func experimentEqualFold() {
+	a := "dicky Novanto"
+	b := "DICKY NOVANTO"
+	log.Printf("%v", strings.EqualFold(a, b))
+}
+
+func main() {
+	experimentEqualFold()
+}
