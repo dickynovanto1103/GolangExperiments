f8e2356cee75dd045a5876ccf244360f1e838a16
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-12-31 22:34:13 +0800

    Add experiment on TestMain

--- testing/main.go
+++ testing/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Print("test")
+}
+
+func Group() int {
+	return 1
 }
--- testing/main_test.go
+++ testing/main_test.go
@@ -1,7 +1,25 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"os"
+	"testing"
 
-func test(t *testing.T) {
+	"github.com/magiconair/properties/assert"
+)
 
+func TestMain(m *testing.M) {
+	log.Printf("before test main")
+	os.Exit(m.Run())
+	log.Printf("done test main")
+}
+
+func TestGroup(t *testing.T) {
+	log.Printf("done1")
+	assert.Equal(t, 1, Group())
+}
+
+func TestGroup2(t *testing.T) {
+	log.Printf("done2")
+	assert.Equal(t, 1, Group())
 }
