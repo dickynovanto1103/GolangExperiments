88549af5f385a8e0490b49a1342c05bcd9df67d2
dickynovanto1103 <dickynovanto1103@gmail.com>
2021-08-09 18:12:21 +0800

    Add ioutil.TempFile and os.Rename and getPid experiment

--- osExperiment/main.go
+++ osExperiment/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -10,4 +12,16 @@ func main() {
 	log.Printf("uid: %v", uid)
 
 	log.Printf("temp dir: %v", os.TempDir())
+
+	file, err := ioutil.TempFile("./", "dicky_*")
+	if err != nil {
+		log.Panicf("error in creating temporary file, err: %v", err)
+	}
+	defer file.Close()
+	log.Printf("filename: %v", file.Name())
+	oldPath := fmt.Sprintf("%v", file.Name())
+	os.Rename(oldPath, "dicky.txt")
+	pid := os.Getpid()
+	log.Printf("pid: %v", pid)
+
 }
