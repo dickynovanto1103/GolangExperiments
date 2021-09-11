881761a14ae2036e8277be4a3a1dee9b3b67dd5b
dickynovanto1103 <dickynovanto1103@gmail.com>
2021-09-11 13:20:06 +0800

    Experiment file.ReadAt from a particular offset

--- fileExperiment/main.go
+++ fileExperiment/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 func main() {
-	experimentWriteToFileUsingFPrintf()
+	// experimentWriteToFileUsingFPrintf()
+	experimentReadFromCertainOffsetInFile()
+}
+
+func experimentReadFromCertainOffsetInFile() {
+	file, err := os.Open("./dicky")
+	if err != nil {
+		log.Panicf("error in opening file, err: %v", err)
+	}
+
+	offset := 1
+	var bytes []byte = make([]byte, 100)
+	n, err := file.ReadAt(bytes, int64(offset))
+	if err != nil && err != io.EOF {
+		log.Panicf("error in read at, err: %v", err)
+	}
+	log.Printf("n: %v bytes: %v, bytes in string: %v", n, bytes, string(bytes))
 }
 
 func experimentWriteToFileUsingFPrintf() {
